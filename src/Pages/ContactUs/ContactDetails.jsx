f8f559ef8a0d937c5c0528bd1ee3bd71bd963c85
Dulmi-2000 <dulmiwijenayaka@gmail.com>
2024-08-23 11:18:07 +0530

    Contact Details page added

---
+++ src/Pages/ContactUs/ContactDetails.jsx
@@ -0,0 +1,144 @@
+import React from 'react'
+import './contactDetails.css'
+import PageIntro from '../../Componenets/PageInto/PageIntro'
+import leader1 from '../../Assets/leader1.png'
+import leader2 from '../../Assets/leader2.png'
+
+function ContactDetails() {
+  return (
+    <>
+          <div className='contact-details'>
+          <PageIntro 
+        title="Contact Details" 
+                  description="At the Sri Lankan Environmental Authority, our leadership team 
+          is dedicated to guiding our efforts in preserving and enhancing the natural 
+          environment. Below, you will find the contact information for our key leaders
+           who oversee various departments and initiatives. Whether you have specific 
+           inquiries, need expert guidance, or wish to collaborate on environmental projects, 
+           our leaders are here to assist you. Please feel free to reach out to the appropriate 
+           contact for your needs."
+        />
+          </div>   
+
+          <div className='container contact-details-body'>
+              <h5 className='contact-details-heading'>Ministry of Environment</h5>
+            <br/><br/>  <hr/>
+          <div className='leader-large'>
+          <div className='leader-cards-section-large'>
+             <div className='img-section'>
+                  <img src={leader1} alt='image' className='leader1-img'/>
+              </div>
+                      <div className='info-section-large'>
+                      <p className='leader-name-large'> Hon. Janaka Wakkumbura, </p>
+                          <h5 className='leader-post-large'>State Minister</h5>
+                          <br></br>
+                      <p className='para-large'><b>Telephone	:</b> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+94 112 034 131</p>
+                      <p className='para-large'><b>Fax	: </b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+94 112 879 954</p>
+              </div>
+              </div>
+
+              <div className='leader-cards-section-large'>
+             <div className='img-section'>
+                  <img src={leader2} alt='image' className='leader1-img'/>
+              </div>
+                  <div className='info-section-large'>
+                  <p className='leader-name-large'>  Mr. Sathasivam Viyalendiran, </p>
+                          <h5 className='leader-post-large'>State Minister</h5>
+                          <br></br>
+                          <p className='para-large'><b>Telephone	:</b> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+94 112 034 131</p>
+                          <p className='para-large'><b>Fax	: </b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+94 112 879 954</p>
+              </div>
+              </div>
+              </div>
+              <div className='leader-large'>
+          <div className='leader-cards-section-large'>
+             <div className='img-section'>
+                  <img src={leader1} alt='image' className='leader1-img'/>
+              </div>
+                      <div className='info-section-large'>
+                      <p className='leader-name-large'> Hon. Janaka Wakkumbura, </p>
+                          <h5 className='leader-post-large'>State Minister</h5>
+                          <br></br>
+                          <p className='para-large'><b>Telephone	:</b> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+94 112 034 131</p>
+                          <p className='para-large'><b>Fax	: </b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+94 112 879 954</p>
+              </div>
+              </div>
+
+              <div className='leader-cards-section-large'>
+             <div className='img-section'>
+                  <img src={leader1} alt='image' className='leader1-img'/>
+              </div>
+                  <div className='info-section-large'>
+                  <p className='leader-name-large'> Hon. Janaka Wakkumbura, </p>
+                          <h5 className='leader-post-large'>State Minister</h5>
+                          <br></br>
+                          <p className='para-large'><b>Telephone	:</b> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+94 112 034 131</p>
+                          <p className='para-large'><b>Fax	: </b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+94 112 879 954</p>
+              </div>
+              </div>
+              </div>
+
+              <br/>
+              <h5 className='contact-details-heading'>Head Office</h5>
+            <br/><br/>  <hr/>
+          <div className='leader-large'>
+          <div className='leader-cards-section-large'>
+             <div className='img-section'>
+                  <img src={leader1} alt='image' className='leader1-img'/>
+              </div>
+                      <div className='info-section-large'>
+                      <p className='leader-name-large'> Hon. Janaka Wakkumbura, </p>
+                          <h5 className='leader-post-large'>State Minister</h5>
+                          <br></br>
+                      <p className='para-large'><b>Telephone	:</b> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+94 112 034 131</p>
+                      <p className='para-large'><b>Fax	: </b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+94 112 879 954</p>
+              </div>
+              </div>
+
+              <div className='leader-cards-section-large'>
+             <div className='img-section'>
+                  <img src={leader2} alt='image' className='leader1-img'/>
+              </div>
+                  <div className='info-section-large'>
+                  <p className='leader-name-large'>  Mr. Sathasivam Viyalendiran, </p>
+                          <h5 className='leader-post-large'>State Minister</h5>
+                          <br></br>
+                          <p className='para-large'><b>Telephone	:</b> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+94 112 034 131</p>
+                          <p className='para-large'><b>Fax	: </b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+94 112 879 954</p>
+              </div>
+              </div>
+              </div>
+              <div className='leader-large'>
+          <div className='leader-cards-section-large'>
+             <div className='img-section'>
+                  <img src={leader1} alt='image' className='leader1-img'/>
+              </div>
+                      <div className='info-section-large'>
+                      <p className='leader-name-large'> Hon. Janaka Wakkumbura, </p>
+                          <h5 className='leader-post-large'>State Minister</h5>
+                          <br></br>
+                          <p className='para-large'><b>Telephone	:</b> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+94 112 034 131</p>
+                          <p className='para-large'><b>Fax	: </b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+94 112 879 954</p>
+              </div>
+              </div>
+
+              <div className='leader-cards-section-large'>
+             <div className='img-section'>
+                  <img src={leader1} alt='image' className='leader1-img'/>
+              </div>
+                  <div className='info-section-large'>
+                  <p className='leader-name-large'> Hon. Janaka Wakkumbura, </p>
+                          <h5 className='leader-post-large'>State Minister</h5>
+                          <br></br>
+                          <p className='para-large'><b>Telephone	:</b> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+94 112 034 131</p>
+                          <p className='para-large'><b>Fax	: </b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+94 112 879 954</p>
+              </div>
+              </div>
+              </div>
+              
+          </div>
+    </>
+  )
+}
+
+export default ContactDetails
