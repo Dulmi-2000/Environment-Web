7ffe004d152206806acd17f552ccc8d48f6631ec
Dulmi-2000 <dulmiwijenayaka@gmail.com>
2024-08-23 09:55:55 +0530

    added

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Home from './Pages/Home';
 import Footer from './Componenets/Footer/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HeaderNav from './Componenets/NavbarHeader/HeaderNav';
-import ContactUs from './Pages/ContactUs/ContactUs';
+import ContactUsInquiries from './Pages/ContactUs/Inquiries';
 import AboutUs from './Pages/AboutUs/AboutUs';
 import Services from './Pages/Services/Services';
 import Divisions from './Pages/Divisions/Divisions';
@@ -14,7 +14,10 @@ import Media from './Pages/Media/Media';
 import Publications from './Pages/Publications/Publications';
 import Regulations from './Pages/Regulations/Regulations';
 import FAQ from './Pages/FAQ/FAQ';
-
+import Complaints from './Pages/Complaints/Complaints'
+import NgoRegistartion from './Pages/NgoRegistration/NgoRegistration'
+import ContactDetails from './Pages/ContactUs/ContactDetails';
+import PhotoGallery from './Pages/Media/PhotoGallery/PhotoGallery'
 
 function App() {
   return (
@@ -27,11 +30,16 @@ function App() {
           <Route path='/AboutUs/AboutUs' element={<AboutUs />} />
           <Route path="/Services/Services" element={<Services />} />
           <Route path="/Divisions/Divisions" element={<Divisions/>} />
-          <Route path='/Media/Media' element={<Media />} />
+          <Route path='/Media/PhotoGallery' element={<PhotoGallery />} />
           <Route path='/Publications/Publications' element={<Publications />} />
           <Route path='/Regulations/Regulations' element={<Regulations />} />
           <Route path='/FAQ/FAQ' element={<FAQ />} />
-          <Route path='/ContactUs/ContactUs' element={<ContactUs />} />
+          <Route path='/ContactUs/ContactUsInquiries' element={<ContactUsInquiries />} />
+          <Route path='/ContactUs/ContactDetails' element={<ContactDetails />} />
+
+          <Route path='/NgoRegistartion/NgoRegistartion' element={<NgoRegistartion />} />
+          <Route path='/Complaints/Complaints' element={<Complaints />} />
+        
         </Routes>
         <Footer />
       </Router>
