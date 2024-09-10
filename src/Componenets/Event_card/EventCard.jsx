e48306277b55b8090ed857cfb950d8bb7861e39f
Dulmi-2000 <dulmiwijenayaka@gmail.com>
2024-09-10 21:30:51 +0530

    conected to databse

--- src/Componenets/Event_card/EventCard.jsx
+++ src/Componenets/Event_card/EventCard.jsx
@@ -1,76 +1,48 @@
-import React from 'react'
-import './eventCard.css'
-import event1 from '../../Assets/event1.jpg'
+import React, { useEffect, useState } from 'react';
+import './eventCard.css';
+import event1 from '../../Assets/event1.jpg';
 
 function EventCard() {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/api');
+        if (!response.ok) throw new Error('Network response was not ok');
+        const data = await response.json();
+        setEvents(data);
+      } catch (error) {
+        console.error('Error fetching events:', error);
+      }
+    };
+    fetchEvents();
+  }, []);
+
+  const renderEventBox = (event, index) => (
+    <div className='event-box' key={index}>
+      <div className='img-section'>
+        <img src={event1} alt='event' className='event-img' />
+      </div>
+      <div className='info-section-large'>
+        <h5 className='event-name'>{event.title}</h5>
+        <p className='para-large'>
+          {event.content}
+          <section className='read-more'>Read More</section>
+        </p>
+      </div>
+    </div>
+  );
+
   return (
-      <>
-          <div className='event-section'>
-        <div className='event'>
-          <div className='event-box'>
-             <div className='img-section'>
-                  <img src={event1} alt='image' className='event-img'/>
-              </div>
-                  <div className='info-section-large'>
-                      
-                      <h5 className='event-name'>Environment Pioneer President’s Medal Awarding Ceremony</h5>
-                      <br/>
-                      <p className='para-large'>The 10th Environment Pioneer President’s Medal Awarding Ceremony 
-                          held at the Bandaranaike Memorial International Conference Hall..
-                      <section className='read-more'>Read More</section>
-                      </p>
-              </div>
-              </div>
-
-              <div className='event-box'>
-             <div className='img-section'>
-                  <img src={event1} alt='image' className='event-img'/>
-              </div>
-                  <div className='info-section-large'>
-                      
-                      <h5 className='event-name'>Environment Pioneer President’s Medal Awarding Ceremony</h5>
-                      <br/>
-                          <p className='para-large'>The 10th Environment Pioneer President’s Medal Awarding Ceremony held at the Bandaranaike Memorial International Conference Hall..
-                      <section className='read-more'>Read More</section>
-                      </p>
-              </div>
-              </div>
-              
-          </div>
-          
-          <div className='event'>
-          <div className='event-box'>
-             <div className='img-section'>
-                  <img src={event1} alt='image' className='event-img'/>
-              </div>
-                  <div className='info-section-large'>
-                      
-                      <h5 className='event-name'>Environment Pioneer President’s Medal Awarding Ceremony</h5>
-                      <br/>
-                      <p className='para-large'>The 10th Environment Pioneer President’s Medal Awarding Ceremony 
-                          held at the Bandaranaike Memorial International Conference Hall..
-                      <section className='read-more'>Read More</section>
-                      </p>
-              </div>
-              </div>
-
-              <div className='event-box'>
-             <div className='img-section'>
-                  <img src={event1} alt='image' className='event-img'/>
-              </div>
-                  <div className='info-section-large'>
-                      
-                      <h5 className='event-name'>Environment Pioneer President’s Medal Awarding Ceremony</h5>
-                      <br/>
-                          <p className='para-large'>The 10th Environment Pioneer President’s Medal Awarding Ceremony held at the Bandaranaike Memorial International Conference Hall..
-                      <section className='read-more'>Read More</section>
-                      </p>
-              </div>
-              </div>
-              </div>
-              </div>
-      </>
-  )
+    <div className='event-section'>
+      {[0, 1, 2].map((i) => (
+        <div className='event' key={i}>
+          {events.slice(i * 2, i * 2 + 2).map(renderEventBox)}
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default EventCard
+export default EventCard;
