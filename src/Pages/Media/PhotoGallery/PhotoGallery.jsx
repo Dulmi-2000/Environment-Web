507146fff1e72b240106d329217dc29ee2892821
Dulmi-2000 <dulmiwijenayaka@gmail.com>
2024-09-12 21:13:41 +0530

    gallery added

--- src/Pages/Media/PhotoGallery/PhotoGallery.jsx
+++ src/Pages/Media/PhotoGallery/PhotoGallery.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './photoGallery.css'
 import leader1 from '../../../Assets/leader1.png'; 
+import PageIntro from '../../../Componenets/PageInto/PageIntro';
 
 const images = [
     { src: leader1, alt: 'Image 1' },
@@ -12,13 +13,21 @@ const images = [
 
 function PhotoGallery() {
     return (
+        <><PageIntro 
+        title="Image Gallery" 
+            description="Welcome to the Image Gallery of the Central Environmental Authority. 
+            Here, you'll find a collection of images capturing our environmental projects, community 
+            initiatives, and efforts in safeguarding Sri Lanka's
+            natural resources. Browse through to witness our journey towards creating a greener, more sustainable future."
+        />
         <div className="container photo-gallery">
             {images.map((image, index) => (
                 <div className="photo" key={index}>
                     <img src={image.src} alt={image.alt} />
                 </div>
             ))}
-        </div>
+            </div>
+            </>
     );
 }
 
